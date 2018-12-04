a1e32654a38aa17c62b97a1cf2a8ec82e5cc717d
Junel Solis <junel.solis@gmail.com>
2018-12-04 22:58:47 +0500

    add foundation framework and some basic css

--- app/resources/views/login.blade.php
+++ app/resources/views/login.blade.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Critical Components CRM</title>
+    <link rel=stylesheet href="{{ asset('css/foundation.min.css')}}" />
+    <link rel='stylesheet' href="{{ asset('css/default.css') }}" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
   </head>
   <body>
     <h1>This is the login page</h1>
