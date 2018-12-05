6d05208e2199aee89611ad06938f4491c1fbe638
Junel Solis <junel.solis@gmail.com>
2018-12-05 10:05:44 +0500

    add redirect logic to send to the correct default dashboard

--- app/app/Http/Controllers/DashboardController.php
+++ app/app/Http/Controllers/DashboardController.php
@@ -7,6 +7,26 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function selectUserRole() {
-      return view('welcome');
+
+      $user_roles = session()->get('logged_in_user_roles');     // get user roles array stored in session
+
+      if ($user_roles->contains('sales')) {
+        return redirect('/sales');
+      }
+
+      if ($user_roles->contains('service')) {
+        return redirect('/service');
+      }
+
+      if ($user_roles->contains('executive')) {
+        return redirect('/exec');
+      }
+
+      if ($user_roles->contains('administrator')) {
+        return redirect('admin');
+      }
+
+      return redirect('/');
+
     }
 }
