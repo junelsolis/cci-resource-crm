c762b6fb98305f628f7f2fb5dabba85c16108b8a
Junel Solis <junel.solis@gmail.com>
2018-12-05 13:09:02 +0500

    created add user form

--- app/resources/views/administrator/main.blade.php
+++ app/resources/views/administrator/main.blade.php
@@ -17,14 +17,38 @@
     <div class='grid-x'>
       <div class='cell medium-6'>
         <div class="card" style="width: 90%;">
-          <h5><strong>Add User</strong></h5>
+          <h5><strong><i class="fas fa-users"></i>&nbsp;Add User</strong></h5>
           <form method='post' action='/admin/user/add'>
             {{ csrf_field() }}
-            <label>Username</label>
-            <input type='text' name='username' required />
-            <label>Full Name</label>
-            <input type='text' name='name' required />
-            <button type='submit' class='button button-primary'>Submit</button>
+            <fieldset class='fieldset'>
+              <legend>
+                User Details
+              </legend>
+              <div class='grid-x grid-padding-x'>
+                <div class='cell small-6'>
+                  <label>Username</label>
+                  <input type='text' name='username' required />
+                </div>
+                <div class='cell small-6'>
+                  <label>Full Name</label>
+                  <input type='text' name='name' required />
+                </div>
+              </div>
+
+            </fieldset>
+            <fieldset class='fieldset'>
+              <legend>
+                Select Roles
+              </legend>
+
+              <input id="checkbox12" type="checkbox"><label for="checkbox12">Sales</label><br />
+              <input id="checkbox22" type="checkbox"><label for="checkbox22">Service</label><br />
+              <input id="checkbox32" type="checkbox"><label for="checkbox32">Executive</label><br />
+              <input id="checkbox33" type="checkbox"><label for="checkbox33">System Administrator</label>
+
+            </fieldset>
+
+            <button type='submit' class='button button-primary'><i class="fas fa-check"></i>&nbsp;Create User</button>
           </form>
         </div>
       </div>
