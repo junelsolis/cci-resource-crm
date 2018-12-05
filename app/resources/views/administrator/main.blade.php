fc0c55b6a51dbea973c5d48db77e63a39ac10a14
Junel Solis <junel.solis@gmail.com>
2018-12-05 12:21:00 +0500

    add ui features to admin dashboard

--- app/resources/views/administrator/main.blade.php
+++ app/resources/views/administrator/main.blade.php
@@ -6,6 +6,7 @@
     <link rel=stylesheet href="{{ asset('css/foundation.min.css')}}" />
     <link rel='stylesheet' href="{{ asset('css/navbar.css') }}" />
     <link rel='stylesheet' href="{{ asset('css/default.css') }}" />
+    <link rel='stylesheet' href="{{ asset('css/admin/main.css') }}" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="{{ asset('js/jquery.js')}}"></script>
@@ -13,7 +14,19 @@
   </head>
   @include('navbar')
   <body>
+    <div class='grid-x'>
+      <div class='cell medium-6'>
+        <div class="card" style="width: 90%;">
+          <h5><strong>Add User</strong></h5>
+        </div>
+      </div>
 
+      <div class='cell medium-6'>
+        <div class="card" style="width: 90%;">
+          <h5>Search Users</h5>
+        </div>
+      </div>
+    </div>
   </body>
   <script>
     $(document).foundation();
