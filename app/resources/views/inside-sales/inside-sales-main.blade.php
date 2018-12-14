84f2d3954757b7bd9a7abdbf06047db87a0dfc22
Junel Solis <junel.solis@gmail.com>
2018-12-14 17:50:51 +0500

    Notes  - Notes can now be added from sidebar by inside sales, provided they are assigned to project

--- app/resources/views/inside-sales/inside-sales-main.blade.php
+++ app/resources/views/inside-sales/inside-sales-main.blade.php
@@ -112,7 +112,7 @@
                     </td>
                     <td id='{{ $i->id}}-engineer'>{{ $i->engineer }}</td>
                     <td id='{{ $i->id}}-contractor'>{{ $i->contractor }}</td>
-                    <td><a class='table-note'>{{ str_limit($i->notes->first()->note,20) }}</a></td>
+                    <td><a class='table-note' data-toggle="{{$i->id}}-info">{{ str_limit($i->notes->first()->note,20) }}</a></td>
                   </tr>
                   @endforeach
                 </tbody>
@@ -324,6 +324,63 @@
             </div>
 
         </div>
+
+        <!-- divs for off-canvas project information -->
+        @foreach ($upcomingProjects as $i)
+        <div class="off-canvas position-right project-info" id="{{$i->id}}-info" data-off-canvas>
+          <h4><span>Project:</span>&nbsp;{{ $i->name }}</h4>
+          <br /><br />
+
+          <form method='post' action='/note/add/{{ $i->id }}'>
+            {{ csrf_field() }}
+            <i class="fas fa-plus"></i>&nbsp;Add Note<br />
+            <textarea name='note' required placeholder='Type note here...'></textarea>
+            <button type='submit' class='primary button'><i class="fas fa-check"></i>&nbsp;Save Note</button>
+          </form>
+          <br />
+          <?php
+            $colorSwitcher = 0;
+            $color = '';
+          ?>
+          @foreach ($i->notes as $note)
+          <?php
+            if ($colorSwitcher >= 5) { $colorSwitcher = 0; }
+
+            if ($colorSwitcher <= 4) {
+              if ($colorSwitcher == 0) {
+                $color = 'style=\'background-color:rgba(46,204,113,0.1)\';';
+              }
+
+              if ($colorSwitcher == 1) {
+                $color = 'style=\'background-color:rgba(241,196,15,0.1)\';';
+              }
+
+              if ($colorSwitcher == 2) {
+                $color = 'style=\'background-color:rgba(155,89,182,0.1)\';';
+              }
+
+              if ($colorSwitcher == 3) {
+                $color = 'style=\'background-color:rgba(231,76,60,0.1)\';';
+              }
+
+              if ($colorSwitcher == 4) {
+                $color = 'style=\'background-color:rgba(241, 196, 15,0.1)\';';
+              }
+
+              $colorSwitcher++;
+            }
+          ?>
+          <div class="note-card" <?php echo $color; ?>>
+            <span>{!! nl2br($note->note) !!}</span>
+            <br /><br />
+            <p style='color:grey;'>
+              <strong>{{ $note->author }}</strong> on {{ $note->date }}
+            </p>
+          </div>
+          @endforeach
+          <span style='color:lightgrey;font-style:italic;text-align:center'>---- End ----</span>
+        </div>
+        @endforeach
       </div>
 
 
@@ -437,7 +494,7 @@
                   <td></td>
                   <td id='{{$i->id}}-all-engineer'>{{ $i->engineer }}</td>
                   <td id='{{$i->id}}-all-contractor'>{{ $i->contractor }}</td>
-                  <td><a class='table-note'>{{ str_limit($i->notes->first()->note,20) }}</a></td>
+                  <td><a class='table-note' data-toggle="{{$i->id}}-info">{{ str_limit($i->notes->first()->note,20) }}</a></td>
                 </tr>
 
                 @endforeach
@@ -645,6 +702,22 @@
             </script>
           </div>
         </div>
+
+        <!-- divs for off-canvas project information -->
+        @foreach ($allProjects as $i)
+        <div class="off-canvas position-right project-info" id="{{$i->id}}-info" data-off-canvas>
+          <h5><span>Project:</span>&nbsp;{{ $i->name }}</h5><br /><br />
+          @foreach ($i->notes as $note)
+          <div class="note-card">
+            <span>{!! nl2br($note->note) !!}</span>
+            <br /><br />
+            <p style='color:grey;'>
+              <strong>{{ $note->author }}</strong> on {{ $note->date }}
+            </p>
+          </div>
+          @endforeach
+        </div>
+        @endforeach
       </div>
     </div>
   </body>
