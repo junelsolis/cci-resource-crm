05b4648af791fd088508fa6c0b28b31dbe2d76ab
Junel Solis <junel.solis@gmail.com>
2018-12-16 13:24:17 +0500

    -- more ui structure for exec dashboard

--- app/app/Http/Controllers/ExecController.php
+++ app/app/Http/Controllers/ExecController.php
@@ -9,12 +9,19 @@ use Carbon\Carbon;
 class ExecController extends Controller
 {
     public function showDashboard() {
-
+      if ($this->checkLoggedIn()) {}
+      else { return redirect('/'); }
 
       $userDetails = $this->getLoggedInUserDetails();
+      $productSalesReps = $this->getProductSalesReps();
+      $insideSalesReps = $this->getInsideSalesReps();
+      $allProjects = $this->getAllProjects();
+
 
       return view('executive/exec-main')
-        ->with('userDetails', $userDetails);
+        ->with('userDetails', $userDetails)
+        ->with('productSalesReps', $productSalesReps)
+        ->with('projects', $allProjects);
     }
 
 
@@ -38,5 +45,199 @@ class ExecController extends Controller
       ]);
 
       return $collect;
+    }
+
+    private function getProductSalesReps() {
+      $sales = DB::table('user_roles')->select('user_id','role')->where('role','product-sales')->distinct()->get();
+      $sales = $sales->pluck('user_id');
+
+      $users = DB::table('users')
+        ->whereIn('id', $sales)
+        ->orderBy('name')
+        ->select('id', 'name')
+        ->get();
+
+
+      foreach ($users as $user) {
+        $lostProjects = $this->getProductSalesRepLostProjects($user->id);
+        $user->lostProjects = $lostProjects;
+
+        $soldProjects = $this->getProductSalesRepSoldProjects($user->id);
+        $user->soldProjects = $soldProjects;
+
+        $upcomingProjects = $this->getProductSalesRepUpcomingProjects($user->id);
+        $user->upcomingProjects = $upcomingProjects;
+      }
+
+      return $users;
+    }
+
+    private function getInsideSalesReps() {
+      /*  Returns a collection of inside salespersons
+      */
+
+      $sales = DB::table('user_roles')->select('user_id','role')->where('role','inside-sales')->distinct()->get();
+      $sales = $sales->pluck('user_id');
+
+      $users = DB::table('users')
+        ->whereIn('id', $sales)
+        ->orderBy('name')
+        ->select('id', 'name')
+        ->get();
+
+      return $users;
+    }
+    
+    private function getProductSalesRepUpcomingProjects($product_sales_id) {
+      $now = Carbon::now();
+      $now->setTimezone('America/New_York');
+
+      $projects = DB::table('projects')
+        ->where('product_sales_id', $product_sales_id)
+        ->where('bid_date', '>=', $now)
+        ->orderBy('bid_date')
+        ->get();
+
+      foreach ($projects as $i) {
+        $date = new Carbon($i->bid_date);
+
+        $i->bidDate = $date->format('m/d/Y');
+      }
+
+      return $projects;
+    }
+
+    private function getProductSalesRepLostProjects($product_sales_id) {
+      $status_id = 5;
+      $projects = DB::table('projects')
+        ->where('product_sales_id', $product_sales_id)
+        ->where('status_id', $status_id)
+        ->get();
+
+      return $projects;
+    }
+
+    private function getProductSalesRepSoldProjects($product_sales_id) {
+      $status_id = 3;
+      $projects = DB::table('projects')
+        ->where('product_sales_id', $product_sales_id)
+        ->where('status_id', $status_id)
+        ->get();
+
+      return $projects;
+    }
+
+    private function getAllProjects() {
+      /*  Get all projects
+          in the last twelve months
+      */
+
+      $now = Carbon::today();
+      $now->setTimezone('America/New_York');
+      $now->subYear();
+
+      $projects = DB::table('projects')
+        ->orderBy('bid_date')
+        ->where('bid_date', '>=', $now)
+        ->get();
+
+      $projects = $this->expandProjectInfo($projects);
+
+      return $projects;
+    }
+
+    private function expandProjectInfo($projects) {
+      /*  Take a collection of projects
+          and expand their information
+      */
+
+      $allStatus = DB::table('project_status')->get();
+      $allInsideSales = $this->getInsideSalesReps();
+      $allProductSales = $this->getProductSalesReps();
+      $allNotes = DB::table('project_notes')->orderBy('created_at','desc')->get();
+
+
+      $now = Carbon::today();
+      $now->setTimezone('America/New_York');
+
+      $nextDay = Carbon::today();
+      $nextDay->setTimezone('America/New_York');
+      $nextDay->addDay();
+
+      $nextWeek = Carbon::today();
+      $nextWeek->setTimezone('America/New_York');
+      $nextWeek->addWeek();
+
+      foreach ($projects as $project) {
+
+        // assign status
+        $status = $allStatus->where('id', $project->status_id)->first();
+        $project->status = $status;
+
+        // format bid date
+        $bidDate = new Carbon($project->bid_date);
+        $date = $bidDate->format('Y-m-d');
+        $project->bidDate = $date;
+
+        // add bid timing
+        if ($bidDate->lessThan($now)) {
+          $bidTiming = 'late';
+          $project->bidTiming = $bidTiming;
+        }
+
+        else if (($bidDate->greaterThanOrEqualTo($now)) && ($bidDate->lessThanOrEqualto($nextWeek))) {
+          $bidTiming = 'soon';
+          $project->bidTiming = $bidTiming;
+        }
+
+        else {
+          $bidTiming = 'ontime';
+          $project->bidTiming = $bidTiming;
+        }
+
+        // add product sales person
+        $productSales = $allProductSales->where('id', $project->product_sales_id)->first();
+
+        $project->productSales = $productSales;
+
+        // add inside sales person
+        $insideSales = $allInsideSales->where('id', $project->inside_sales_id)->first();
+        $project->insideSales = $insideSales;
+
+        // format amount
+        $project->amount = '$' . number_format($project->amount);
+
+        // append project notes
+        $notes = $allNotes->where('project_id', $project->id);
+
+        foreach ($notes as $note) {
+
+          // add formatted date to note
+          $date = new Carbon($note->created_at);
+          $date->setTimezone('America/New_York');
+
+          $note->date = $date->format('D, m/d/Y h:i:s a');
+
+          // add note author name
+          $author = $allInsideSales->where('id', $note->last_updated_by_id)->first();
+          if (empty($author)) {
+            $author = $allProductSales->where('id', $note->last_updated_by_id)->first();
+          }
+
+          $note->author = $author->name;
+        }
+
+        $project->notes = $notes;
+      }
+
+      return $projects;
+    }
+
+    private function checkLoggedIn() {
+      if (session()->has('logged_in_user_id') && session('logged_in_user_roles')->contains('executive')) {
+        return true;
+      }
+
+      return false;
     }
 }
--- app/resources/views/executive/exec-main.blade.php
+++ app/resources/views/executive/exec-main.blade.php
@@ -5,7 +5,6 @@
     <title>Executive | CCI Tracker</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href='{{ asset('css/bootstrap.css') }}'rel='stylesheet' />
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/zf/dt-1.10.18/datatables.min.css"/>
     <link rel=stylesheet href="{{ asset('css/app.css') }}" />
     <link rel='stylesheet' href="{{ asset('css/navbar.css') }}" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
@@ -13,12 +12,8 @@
     <script src="{{ asset('js/jquery.js')}}"></script>
     <script src="{{ asset('js/foundation.min.js')}}"></script>
     <script src="{{ asset('js/Chart.min.js')}}"></script>
-    <!-- <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script> -->
-    <!-- <script src='{{ asset('js/bootstrap.min.js')}}'></script>
-    <link href="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet"/>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/zf/dt-1.10.18/datatables.min.js"></script> -->
-
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/zf/dt-1.10.18/datatables.min.css"/>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/zf/dt-1.10.18/datatables.min.js"></script>
   </head>
   @include('navbar')
   <body>
@@ -27,7 +22,7 @@
       <!-- SALES SECTION -->
       <div class='cell small-12'>
         <div class='card'>
-          <h5><strong>Sales</strong></h5>
+          <h5><strong><i class="fas fa-dollar-sign"></i>&nbsp;Sales</strong></h5>
 
           <div class='grid-x'>
             <div class='cell medium-6 large-4'>
@@ -43,6 +38,9 @@
             <div class='cell medium-6 large-2' style='text-align:center;'>
               <span style='color:#3e95cd;font-size:40px;font-weight:bold;'>$2,350,300</span>
               <h5>Projected Sales<br />Next 6 Months</h5>
+            </div>
+            <div class='cell medium-6 large-4'>
+              <canvas id='lost-bids'></canvas>
             </div>
           </div>
         </div>
@@ -108,38 +106,266 @@
           });
 
 
+          var ctx = document.getElementById('lost-bids').getContext('2d');
+          var salesPastYear = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: [
+                'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug','Sep','Oct','Nov','Dec'
+              ],
+              datasets: [{
+                data: [
+                  11000, 7500, 30000, 25000, 6500, 24000, 33000, 12000, 34000, 12000, 4000, 21000
+                ],
+                borderColor: 'rgba(127, 140, 141,1.0)',
+                fill: true,
+                backgroundColor: "rgba(127, 140, 141,0.2)"
+              }]
+            },
+            options: {
+              maintainAspectRatio: false,
+              title: {
+                display: true,
+                text: 'Lost Bids (Last 12 months)'
+              },
+              legend: {
+                display: false,
+              }
+            }
+          });
+
+
+
         </script>
       </div>
 
-      <!-- PROJECTS SECTIONS -->
+      <!-- PROJECT STATS SECTION -->
       <div class='cell small-12'>
         <div class='card'>
-          <h5><strong>Projects</strong></h5>
+          <h5><strong><i class="fas fa-project-diagram"></i>&nbsp;Project Stats</strong></h5>
           <div class='grid-x'>
             <div class='cell medium-4'>
-              Project Counts (Last 12 months)
+              <canvas id="project-counts"></canvas>
             </div>
             <div class='cell medium-4'>
-              Project status
+              <canvas id="project-status"></canvas>
             </div>
             <div class='cell medium-4'>
               Top Grossing Projects (Last 12 months)
             </div>
           </div>
         </div>
+
+        <!-- javascript for project charts -->
+        <script>
+          var ctx = document.getElementById("project-counts").getContext('2d');
+          var projectCounts = new Chart(ctx, {
+              type: 'bar',
+              data: {
+                  labels: [ 'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+                  datasets: [{
+                      data: [30,23,44,88,59,28,36,64,75,67,26,48],
+                      backgroundColor: [
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(255, 159, 64, 0.2)',
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(255, 159, 64, 0.2)'
+                      ],
+                      borderColor: [
+                          'rgba(255,99,132,1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)',
+                          'rgba(255,99,132,1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)'
+                      ],
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                maintainAspectRatio: false,
+                title: {
+                  display: true,
+                  text: 'Project Counts (Last 12 months)'
+                },
+                legend: {
+                  display: false,
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+              }
+          });
+
+
+          var ctx = document.getElementById("project-status").getContext('2d');
+          var projectStatus = new Chart(ctx, {
+              type: 'doughnut',
+              data: {
+                  // labels: ["New","Quoted","Sold","Engineered","Lost"],
+                  labels: ['New','Quoted','Engineered','Sold','Lost'],
+                  datasets: [{
+                      data: [95,64,34,128,17],
+                      backgroundColor: [
+                          'rgba(243,156,18,0.6)',
+                          'rgba(41,128,185,0.6)',
+                          'rgba(39,174,96,0.6)',
+                          'rgba(142,68,173,0.6)',
+                          'rgba(44,62,80,0.6)',
+                      ],
+                      // borderColor: [
+                      //   'rgba(243,156,18,1)',
+                      //   'rgba(41,128,185,1)',
+                      //   'rgba(39,174,96,1)',
+                      //   'rgba(142,68,173,1)',
+                      //   'rgba(44,62,80,1)',
+                      // ],
+                      borderWidth: 1
+                  }]
+
+              },
+              options: {
+                maintainAspectRatio: false,
+                title: {
+                  display: true,
+                  text: 'Project Status (Up to last 12 months)'
+                },
+                legend: {
+                  display: true,
+                  position: 'right',
+                },
+              }
+          });
+        </script>
       </div>
 
+
+      <!-- PROJECTS SECTION -->
+      <div class='cell medium-12'>
+        <div class='card'>
+          <h5><strong>Projects</strong></h5>
+          <div class='table-scroll'>
+            <table id='projectsTable' class='unstriped display'>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Status</th>
+                  <th>Bid Date</th>
+                  <th>Manufacturer</th>
+                  <th>Product</th>
+                  <th>Product Sales</th>
+                  <th>Inside Sales</th>
+                  <th>Amount</th>
+                  <th>APC OPP ID</th>
+                  <th>Quote Link</th>
+                  <th>Engineer</th>
+                  <th>Contractor</th>
+                  <th>Note</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($projects as $i)
+                <tr>
+                  <td></td>
+                  <td>{{ $i->name }}</td>
+                  <td
+                    <?php
+                      if ($i->status->status == 'New') { echo 'class=\'status-new\''; }
+                      if ($i->status->status == 'Engineered') { echo 'class=\'status-engineered\''; }
+                      if ($i->status->status == 'Sold') { echo 'class=\'status-sold\''; }
+                      if ($i->status->status == 'Quoted') { echo 'class=\'status-quoted\''; }
+                      if ($i->status->status == 'Lost') { echo 'class=\'status-lost\''; }
+                    ?>
+                  >{{ $i->status->status }}
+                  </td>
+                  <td>{{ $i->bidDate }}</td>
+                  <td>{{ $i->manufacturer}}</td>
+                  <td>{{ $i->product}}</td>
+                  <td>{{ $i->productSales->name }}</td>
+                  <td>{{ $i->insideSales->name }}</td>
+                  <td>{{ $i->amount }}</td>
+                  <td>{{ $i->apc_opp_id }}</td>
+                  <td>
+                    @if (isset($i->invoice_link))
+                    <a href='{{ $i->invoice_link }}' target='_blank'><i class="fas fa-link"></i></i></a>
+                    @endif
+                  </td>
+                  <td>{{ $i->engineer }}</td>
+                  <td>{{ $i->contractor }}</td>
+                  <td><a class='table-note' data-toggle="{{$i->id}}-info">{{ str_limit($i->notes->first()->note,20) }}</a></td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+
+            <!-- javascript for table -->
+            <script>
+              $(document).ready(function() {
+
+                $('#projectsTable').DataTable( {
+                  "order": [[ 3, 'desc']],
+                  'pageLength': 10,
+                });
+              });
+            </script>
+
+          </div>
+        </div>
+      </div>
 
       <!-- PEOPLE SECTION -->
       <div class='cell small-12'>
         <div class='card'>
-          <h5><strong>People</strong></h5>
+          <h5><strong><i class="fas fa-users"></i>&nbsp;People</strong></h5>
+          <div class='table-scroll'>
+            <table class='unstriped'>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Upcoming Projects</th>
+                  <th>Projected Sales (Next 6 Months)</th>
+                  <th>Total Sales (Last 12 Months)</th>
+                  <th>Total Projects (Last 12 Months)</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($productSalesReps as $i)
+                <tr>
+                  <td>{{ $i->name }}</td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
+
+
     </div>
   </body>
   @include('footer')
   <script>
     $(document).foundation();
   </script>
+
+
 </html>
