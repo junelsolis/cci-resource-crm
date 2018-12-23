2c6880078516fd601c64c2997ddc48ac35e5f49c
Junel Solis <junel.solis@gmail.com>
2018-12-23 23:36:24 +0500

    passing unit test of InsideSalesUser model

--- app/app/InsideSalesUser.php
+++ app/app/InsideSalesUser.php
@@ -3,10 +3,91 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use Carbon\Carbon;
+use App\Project;
+use DB;
 
-class InsideSalesUser extends Model
+class InsideSalesUser extends User
 {
 
+    public $userDetails;
 
+    public function projectsThisYear() {
+      $thisYear = Carbon::now()->subYear();
+      $thisYear->format('Y-m-d');
+
+      $projects = Project::where('inside_sales_id', $this->id)
+        ->where('bid_date', '>=', $thisYear)
+        ->orderBy('bid_date', 'desc')
+        ->get();
+
+      return $projects;
+    }
+
+    public function upcomingProjects() {
+
+      $thisYear = Carbon::now()->subYear();
+
+      $projects = Project::where('inside_sales_id', $this->id)
+        ->where([
+          [ 'status_id', '!=', 3],  // Sold
+          [ 'status_id', '!=', 5]   // Lost
+        ])
+        ->where('bid_date', '>=', $thisYear)
+        ->orderBy('bid_date','desc')->get();
+
+      $now = Carbon::now();
+      //$now->setTimezone('America/New_York');
+
+      foreach ($projects as $key => $item) {
+        $bid_date = new Carbon($item->bid_date);
+
+        $status = $item->status_id;
+
+        if ($now->greaterThan($bid_date)) {
+
+          if ($status == 1 || $status == 4) {
+
+            continue;
+          }
+
+          else { $projects->forget($key); }
+        }
+
+      }
+
+      $projects = $projects->reverse();
+      //$projects = $projects->take(5);
+
+      return $projects;
+
+    }
+
+    public function ongoingProjects() {
+      $status_ids = [1,2,4];
+
+      $projects = Project::where('inside_sales_id', $this->id)
+        ->whereIn('status_id', $status_ids)
+        ->get();
+
+
+      return $projects;
+    }
+
+    public function userDetails() {
+      $user = User::where('id', session('logged_in_user_id'))->first();
+
+      $collect = collect();
+
+      $collect->put('name', $user->name);
+      $collect->put('role', 'Inside Sales');
+
+      $this->userDetails = $collect;
+      return $this->userDetails;
+    }
+
+
+    protected $fillable = ['name','username'];
     protected $table = 'users';
 }
--- app/tests/Unit/InsideSalesUserTest.php
+++ app/tests/Unit/InsideSalesUserTest.php
@@ -6,15 +6,141 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\InsideSalesUser;
+use Carbon\Carbon;
+use App\Project;
+
+
 class InsideSalesUserTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
+
+    use RefreshDatabase;
+
+    protected $user;
+
+    public function setUp() {
+      parent::setUp();
+
+      $this->session(['logged_in_user_id' => 77]);
+
+      $this->user = factory('App\InsideSalesUser')->create([
+        'id' => 77,
+        'name' => 'Samuel Langhorne Clemens'
+      ]);
     }
+
+
+    /** @test */
+    public function a_user_has_projects_this_year() {
+
+      // projects within this year
+      factory('App\Project',30)->create([
+        'bid_date' => Carbon::now()->subMonths(rand(0,11)),
+        'inside_sales_id' => $this->user->id
+      ]);
+
+      // projects more than a year old
+      factory('App\Project',15)->create([
+        'bid_date' => Carbon::now()->subMonths(rand(12,20)),
+        'inside_sales_id' => $this->user->id
+      ]);
+
+      $this->assertTrue($this->user->projectsThisYear()->count() == 30);
+    }
+
+
+    /** @test */
+    public function a_user_has_upcomingProjects() {
+
+      // new projects
+      factory('App\Project', 15)->create([
+        'inside_sales_id' => 77,
+        'bid_date' => Carbon::now()->addMonths(rand(1,11)),
+        'status_id' => 1
+      ]);
+
+      factory('App\Project', 10)->create([
+        'inside_sales_id' => 77,
+        'bid_date' => Carbon::now()->subDays(rand(1,60)),
+        'status_id' => 1
+      ]);
+
+      // quoted
+      factory('App\Project', 10)->create([
+        'inside_sales_id' => 77,
+        'bid_date' => Carbon::now()->subWeeks(rand(1,5)),
+        'status_id' => 2
+      ]);
+
+      // engineered
+      factory('App\Project', 5)->create([
+        'inside_sales_id' => 77,
+        'bid_date'  => Carbon::now()->addDays(rand(1,30)),
+        'status_id' => 4
+      ]);
+
+      // sold
+      factory('App\Project', 30)->create([
+        'inside_sales_id' => 77,
+        'bid_date' => Carbon::now()->addDays(rand(1,30)),
+        'status_id' => 3
+      ]);
+
+      // lost
+      factory('App\Project', 10)->create([
+        'inside_sales_id' => 77,
+        'bid_date' => Carbon::now()->addDays(rand(1,30)),
+        'status_id' => 5
+      ]);
+
+
+      $this->assertTrue($this->user->upcomingProjects()->count() == 30);
+
+
+    }
+
+
+    /** @test */
+    public function a_user_has_ongoing_projects() {
+      factory('App\Project',10)->create([
+        'inside_sales_id' => 77,
+        'status_id' => 1,
+        'bid_date' => Carbon::now()->addDays(rand(0,10))
+      ]);
+
+      factory('App\Project',15)->create([
+        'inside_sales_id' => 77,
+        'status_id' => 2,
+        'bid_date' => Carbon::now()->addDays(rand(1,10))
+      ]);
+
+      factory('App\Project',10)->create([
+        'inside_sales_id' => 77,
+        'status_id' => 2,
+        'bid_date' =>  Carbon::now()->subDays(rand(1,30))
+      ]);
+
+      factory('App\Project',20)->create([
+        'inside_sales_id' => 77,
+        'status_id' => 4,
+        'bid_date' => Carbon::now()->addDays(rand(0,10))
+      ]);
+
+      factory('App\Project',15)->create([
+        'inside_sales_id' => 77,
+        'status_id' => 4,
+        'bid_date' => Carbon::now()->subDays(rand(1,30))
+      ]);
+
+      $this->assertTrue($this->user->ongoingProjects()->count() == 70);
+    }
+
+
+    /** @test */
+    public function a_user_has_details() {
+
+      $this->assertTrue($this->user->userDetails()->get('name') == 'Samuel Langhorne Clemens');
+      $this->assertTrue($this->user->userDetails()->get('role') == 'Inside Sales');
+    }
+
 }
